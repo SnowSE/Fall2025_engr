3bcabbf4d10d6b34a863a355f326c8d64a29e7da
Dallan Simper <dallan.simper@gmail.com>
2025-08-25 14:40:04 -0600

    First day

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	cout << "We are going to multiply numbers. " << endl;
+	cout << "4*3=" << 4*3 <<endl;
+
+	int a = 0;
+	cin >> a;
+
+	a = 67 * a;
+
+	cout << "a= " << a << endl;
+
+	return 0;
+}
+
