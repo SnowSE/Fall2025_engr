7883463d80367cabc13ec08016b2eff2a85734ab
Dallan Simper <dallan.simper@gmail.com>
2025-09-05 14:46:05 -0600

    chained else/if

--- Source.cpp
+++ Source.cpp
@@ -2,27 +2,34 @@
 #include <string>
 using namespace std;
 
-
-
 int main() {
 
-	// Single line comment
 
-	int s
-		/*
-			Multiline comment
-		*/
-		= 12;
+	int a;
+	cin >> a;
 
-	double r //comment
-		= 4.5 * 78;
+	if (a > 1)
+		if (a < 200)
+			if (a == 100)
+				a++;
+			else
+				a--;
 
-
-	/*
-	r = s * r;
-	
-	//*/
-	cout << r;
+	if (a == 0) 
+	{
+		cout << "it's 0" << endl;
+	}
+	else if (a > 12) {
+		cout << "That's a big number." << endl;
+		if (a > 100) {
+			cout << "That's a really big number." << endl;
+		}
+	}
+	else 
+	{
+		cout << "not 0" << endl;
+	}
+	cout << a << endl;
 
 }
 
