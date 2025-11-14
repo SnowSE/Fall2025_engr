20876180bdb308ca4fd808353b883fac076f8922
Dallan Simper <dallan.simper@gmail.com>
2025-11-14 14:51:54 -0700

    point test qeustion

--- Program.cpp
+++ Program.cpp
@@ -7,38 +7,14 @@
 
 using namespace std;
 
-bool isLoShuSquare(int arr[3][3]) {
-
-	int sum = arr[0][0] + arr[1][1] + arr[2][2];
-	int sum1 = arr[0][2] + arr[1][1] + arr[2][0];
-
-	if (sum != sum1)
-		return false;
-
-	for (int i = 0; i < 3; i++)
-	{
-		int sumH = arr[0][i] + arr[1][i] + arr[2][i];
-		int sumV = arr[i][0] + arr[i][1] + arr[i][2];
-
-		if (sumH != sum)
-			return false;
-		if (sumV != sum)
-			return false;
-
-	}
-
-
-	return true;
-}
 
 
 int main() {
 
-	int * ptr = new int[100];
+	int a[3] = { 7,12,16 };
+	int* ptr = a;
 
-	cout << "Memory Location: " << ptr << endl;
-	cout << "First Value: " << *ptr << endl;
-
+	cout << ptr;
 
 	return 0;
 }
