536ef85458d0d31e7536ae34fa86aa499128e2b9
Dallan Simper <dallan.simper@gmail.com>
2025-12-05 14:40:03 -0700

    pointes

--- Program.cpp
+++ Program.cpp
@@ -7,39 +7,31 @@
 
 using namespace std;
 
-struct Point { 
+struct Point {
 	int X; 
-	int Y; 
+	int Y;
 };
 
-void printArray(int arr[], int size) {
-	for (int i = 0; i < size; i++)
-	{
-		cout << arr[i] << ", ";
-	}
-	cout << endl;
+void add(int a, int b, int* sum) {
+	*sum = a + b;
 }
 
 int main() {
-	const int SIZE = 10;
-	int list[] = { 25,34, 4,17,44,5,7,9,18,3 };
+	
+	int a = 7, b = 12;
+	int c = 0;
+	int* ptr = &c;
 
+	add(a, b, &c);
 
+	cout << c << endl;
 
+	a = 55; 
+	b = 13;
 
-	printArray(list, SIZE);
-	for (int j = 0; j < SIZE - 1; j++) {
-		for (int i = 0; i < SIZE - 1 - j; i++)
-		{
-			if (list[i] > list[i + 1]) {
-				int temp = list[i];
-				list[i] = list[i + 1];
-				list[i + 1] = temp;
-			}
-		printArray(list, SIZE);
-		}
-		cout << j << endl;
-	}
+	add(a, b, ptr);
+
+	cout << *ptr << endl;
 
 	return 0;
 }
